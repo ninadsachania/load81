cf5b0f164f905072e9cff88fb3297c99fbec4dd7
Ninad Sachania <ninad.sachania@gmail.com>
2026-02-03 21:08:41 +0530

    Stop build script on first error.

--- build.bat
+++ build.bat
@@ -1,23 +1,29 @@
 @echo off
+setlocal
 
-if not exist build mkdir build
+if not exist build mkdir build || exit /b 1
 
-cl /O2 /W4 /Fo:build\ /Fe:build\load81.exe main.c ^
-  /I.\libs\SDL2\include ^
-  /I.\libs\SDL2_gfx\include ^
-  /I.\libs\SDL2_image\include ^
-  /I.\libs\lua\include ^
+pushd build || exit /b 1
+
+cl /nologo /TC /Zi /Fe:load81.exe ..\main.c ^
+  /I..^
+  /I..\libs\SDL2\include ^
+  /I..\libs\SDL2_gfx\include ^
+  /I..\libs\SDL2_image\include ^
+  /I..\libs\lua\include ^
   /link /SUBSYSTEM:CONSOLE ^
-  /LIBPATH:libs\SDL2\lib\x64 ^
-  /LIBPATH:libs\SDL2_gfx\lib\x64 ^
-  /LIBPATH:libs\SDL2_image\lib\x64 ^
-  /LIBPATH:libs\lua ^
-  SDL2.lib SDL2_image.lib SDL2_gfx.lib lua51.lib SDL2main.lib Shell32.lib
+  /LIBPATH:..\libs\SDL2\lib\x64 ^
+  /LIBPATH:..\libs\SDL2_gfx\lib\x64 ^
+  /LIBPATH:..\libs\SDL2_image\lib\x64 ^
+  /LIBPATH:..\libs\lua ^
+  SDL2.lib SDL2_image.lib SDL2_gfx.lib lua51.lib SDL2main.lib Shell32.lib || exit /b 1
 
-copy libs\SDL2\lib\x64\SDL2.dll build\
-copy libs\SDL2_image\lib\x64\SDL2_image.dll build\
-copy libs\SDL2_gfx\lib\x64\SDL2_gfx.dll build\
-copy libs\lua\lua51.dll build\
+copy ..\libs\SDL2\lib\x64\SDL2.dll             . || exit /b 1
+copy ..\libs\SDL2_image\lib\x64\SDL2_image.dll . || exit /b 1
+copy ..\libs\SDL2_gfx\lib\x64\SDL2_gfx.dll     . || exit /b 1
+copy ..\libs\lua\lua51.dll                     . || exit /b 1
+
+popd
 
 echo.
 echo Build complete!
